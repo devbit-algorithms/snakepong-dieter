09e8cfc042f5ea7047f48a0cd5752df0b2ba7817
Dieter Dewachter <dieter.dewachter@student.vives.be>
2020-12-23 14:06:05 +0100

    Adjusted candy class

--- candyBall.py
+++ candyBall.py
@@ -1,4 +1,9 @@
 class CandyBall:
     def __init__(self, coordinate):
+        self.move(coordinate)
+    
+    def coordinate(self):
+        return self.__coordinate
+    
+    def move(self, coordinate):
         self.__coordinate = coordinate
-
